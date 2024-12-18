101372cc90fd968c9d3c9467f62c036b8131bfb0
Kateryna <kat.rin47u@gmail.com>
2024-12-18 18:03:14 +0000

    Solution

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -1,4 +1,3 @@
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
@@ -6,5 +5,4 @@ from django.urls.conf import include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/cinema/", include("cinema.urls"), name="cinema"),
-    path("__debug__/", include("debug_toolbar.urls")),
 ]
